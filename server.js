caf627c4c289b533afafe027ee5deafbb825485d
thanapoomo58 <35727362+thanapoomo58@users.noreply.github.com>
2018-10-11 14:37:00 +0700

    Add files via upload

---
+++ server.js
@@ -0,0 +1,7 @@
+const express = require("express")
+const app = express()
+const port = 8000
+
+app.use(express.static(__dirname));
+app.get('/style.css',(req,res)=>res.sendFile(__dirname+"/"+"style.css"))
+app.listen(port, () => console.log('app listening on port'+port))
